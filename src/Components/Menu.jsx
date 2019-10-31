46f3e19561a0a79cd0a731fd05281fd28c05c090
Nor014 <metal_14@mail.ru>
2019-10-31 21:48:46 +0700

    first commit

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
+
+export default function Menu() {
+
+  return (
+    <nav className="menu">
+      <NavLink className="menu__item" activeClassName='menu__item-active' exact to="/">Главная</NavLink>
+      <NavLink className="menu__item" activeClassName='menu__item-active' exact to="/drift">Дрифт-такси</NavLink>
+      <NavLink className="menu__item" activeClassName='menu__item-active' exact to="/timeattack">Time Attack</NavLink>
+      <NavLink className="menu__item" activeClassName='menu__item-active' exact to="/forza">Forza Karting</NavLink>
+    </nav>
+  )
+
+}
